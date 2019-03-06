489f26e3b1cf83114a73f4fb4a8b0bdbd2778b48
Madi <madelyn.e.helton@gmail.com>
2019-03-05 19:44:24 -0600

    fixed eslint add on

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+
+  state= {
+    incorrect: [],
+    picked: []
+  };
+
+
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    );
+  }
+};
+
+export default Home;
